c243636b5e5b35328f5f5219bbc7ac3d7c5124f8
Faustino50 <50456329+Faustino50-Cattaro@users.noreply.github.com>
2019-06-20 19:32:40 +0200

    rotate2.py
    rotate.py but without rotate_letter function and with a personal request

---
+++ code/rotate2.py
@@ -0,0 +1,28 @@
+"""
+Alex Fegatilli
+The same rotate.py but without rotate_letter function and with a personal request
+"""
+
+def cesare(word, number):
+    n = 0
+    start = 0
+    ciphertext = ""
+    while n < len(word):
+        if word[n].isupper():
+            start = 65 # ord('A')
+        elif word[n].islower():
+            start = 97 # ord('a')
+
+        letter = ord(word[n]) - start
+        c = ((letter+number) % 26) + start
+        ciphertext += chr(c)
+        n+=1
+    return ciphertext
+
+def request():
+    word = input("Write the plaintext: ")
+    number = int(input("N. of shifts: "))
+    print("\nPlaintext: ",word)
+    print("Ciphertext: ",cesare(word,number))
+
+request()
