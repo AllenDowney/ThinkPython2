bbbe63e49f5e969318e814f224c4b4b01b6233e3
Faustino50 <50456329+Faustino50-Cattaro@users.noreply.github.com>
2019-06-20 21:59:03 +0200

    Update rotate2.py

--- code/rotate2.py
+++ code/rotate2.py
@@ -2,7 +2,6 @@
 Alex Fegatilli
 The same rotate.py but without rotate_letter function and with a personal request
 """
-
 def cesare(word, number):
     n = 0
     start = 0
@@ -10,12 +9,16 @@ def cesare(word, number):
     while n < len(word):
         if word[n].isupper():
             start = 65 # ord('A')
+            letter = ord(word[n]) - start
+            c = ((letter+number) % 26) + start
+            ciphertext += chr(c)
+        
         elif word[n].islower():
             start = 97 # ord('a')
-
-        letter = ord(word[n]) - start
-        c = ((letter+number) % 26) + start
-        ciphertext += chr(c)
+            letter = ord(word[n]) - start
+            c = ((letter+number) % 26) + start
+            ciphertext += chr(c)
+            
         n+=1
     return ciphertext
 
